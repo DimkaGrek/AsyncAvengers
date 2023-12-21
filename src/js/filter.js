ac0d270e18ec87315948818aa033223625a01854
Natali258 <natalya.holyavko@gmail.com>
2023-12-21 18:22:23 +0200

    filters-js-style
    будуть ще правки, зберігаю поки частину коду.

--- src/js/filter.js
+++ src/js/filter.js
@@ -1,1 +1,51 @@
-console.log('Hello from filter.js');
+import FoodApi from './FoodApi';
+
+let params = {
+  keyword: null,
+  category: null,
+  page: 1,
+  limit: 6,
+};
+
+const refs = {
+  ftInput: document.querySelector('.filters-input'),
+  ftSelect: document.querySelector('.select_list'),
+  ftBtn: document.querySelector('.select-btn'),
+};
+
+// =======================Create-Categories========================
+
+const getProductsCategories = async () => {
+  const categories = await FoodApi.getProductsCategories();
+
+  const createOptions = categories.map(el => {
+    return `<li class="select-li">${el}</li>`;
+  });
+  createOptions.unshift(`<li class="select-li">Categories</li>`);
+  refs.ftSelect.insertAdjacentHTML('beforeend', createOptions.join('\n'));
+};
+getProductsCategories();
+
+refs.ftBtn.addEventListener('click', openCategories);
+refs.ftSelect.addEventListener('click', choiceCategories);
+
+function openCategories() {
+  refs.ftSelect.classList.toggle('is-open');
+}
+
+async function choiceCategories(li) {
+  const liValue = li.target.textContent;
+  refs.ftBtn.innerHTML = liValue;
+  refs.ftSelect.classList.remove('is-open');
+  params.category = liValue;
+  const productResoult = await FoodApi.getProductsByFilter(params);
+}
+
+// =======================Create-Search-Input========================
+const getSearch = e => {
+  const searchValue = e.target.value;
+  console.log(searchValue);
+  params.keyword = searchValue;
+  localStorage.setItem('searchKey', JSON.stringify(params));
+};
+refs.ftInput.addEventListener('input', getSearch);
