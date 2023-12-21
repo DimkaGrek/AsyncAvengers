c97d91f78f09f784912ffbb5d1496c75c392ec49
Niukalo Tetiana <tanusha.17121994@gmail.com>
2023-12-21 18:49:00 +0000

    email check

--- src/js/footer.js
+++ src/js/footer.js
@@ -1,1 +1,26 @@
-console.log('Hello from footer.js');
+import FoodApi from './FoodApi';
+import { openModalEmailSub, openModalEmailSubError } from './modal/modal';
+
+const refs = {
+  form: document.querySelector('.js-footer-form'),
+};
+
+const onFormSubmit = async event => {
+  event.preventDefault();
+
+  const { email } = event.target;
+
+  try {
+    const message = await FoodApi.createSubscription(email.value);
+    openModalEmailSub();
+  } catch (error) {
+    if (error.response.status === 409) {
+      openModalEmailSubError();
+    } else {
+      console.log(error);
+    }
+  }
+  event.target.reset();
+};
+
+refs.form.addEventListener('submit', onFormSubmit);
